de40e3fdecc1019c7ecae37f5a8ca9b3d3a9252d
IlyaLundin <gospodinls666@mail.ru>
2023-03-31 22:59:22 +0300

    Commit1

---
+++ Pages/PageQWERTY.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalendarNotes.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageQWERTY.xaml
+    /// </summary>
+    public partial class PageQWERTY : Page
+    {
+        public PageQWERTY()
+        {
+            InitializeComponent();
+        }
+    }
+}
